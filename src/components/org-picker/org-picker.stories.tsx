7220926f89bc59e6b84ec19e3f1fee0ce523009b
Jacques <jdukes22@gmail.com>
2019-03-11 18:28:52 -0500

    Added org-picker.tsx

---
+++ src/components/org-picker/org-picker.stories.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { object } from '@storybook/addon-knobs';
+import { action } from '@storybook/addon-actions';
+import OrgPicker from './org-picker';
+import StorybookUI from '../../utils/storybook-ui';
+
+let organizations = [
+  {
+    name: 'NBC Universal',
+    logo: 'https://upload.wikimedia.org/wikipedia/commons/3/3f/NBC_logo.svg',
+    isCurrent: true,
+    onClick: action('onClick'),
+  },
+  {
+    name: 'Dotdash',
+    logo:
+      'https://upload.wikimedia.org/wikipedia/commons/6/64/Dotdash_logo.png',
+    isCurrent: false,
+    onClick: action('onClick'),
+  },
+  {
+    name: 'Fusion Media Group',
+    logo:
+      'https://pbs.twimg.com/profile_images/736209330534526977/HoA165an_400x400.jpg',
+    isCurrent: false,
+    onClick: action('onClick'),
+  },
+  {
+    name: 'Ziff Davis',
+    logo:
+      'https://pbs.twimg.com/profile_images/692109861551751178/aLLlRLwq_400x400.png',
+    isCurrent: false,
+    onClick: action('onClick'),
+  },
+];
+
+storiesOf('Navigation', module)
+  .addDecorator((story) => <StorybookUI>{story()}</StorybookUI>)
+  .add('OrgPicker', () => (
+    <OrgPicker
+      onSettingsClick={action('onSettingsClick')}
+      organizations={object('organizations', organizations)}
+    />
+  ));
